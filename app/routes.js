eecf30d38e3c26ba6c238756c7ae624adbe9122f
Richárd Németh <nemethricsi@gmail.com>
2020-01-23 14:06:48 +0100

    add response

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const request = require('request');
 const app = express();
 let logs = [];
+
 
 app.use(express.json());
 
@@ -36,9 +38,16 @@ app.post('/webhook', (req, res) => {
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
       logs.push(webhook_event);
+
       let sender_psid = webhook_event.sender.id;
       console.log('Sender PSID: ' + sender_psid); 
       logs.push(sender_psid);
+
+      if (webhook_event.message) {
+        handleMessage(sender_psid, webhook_event.message);        
+      } else if (webhook_event.postback) {
+        handlePostback(sender_psid, webhook_event.postback);
+      }
     });
 
     res.status(200).send('EVENT_RECEIVED');
@@ -47,5 +56,47 @@ app.post('/webhook', (req, res) => {
   }
 
 });
+
+// Handles messages events
+const handleMessage = (sender_psid, received_message) => {
+  let response;
+
+  if (received_message.text) {
+    response = {
+      "text": `You sent the message: "${received_message.text}". Now send me an image!`,
+    };
+  }
+
+  callSendAPI(sender_psid, response);
+}
+
+// Handles messaging_postbacks events
+const handlePostback = (sender_psid, received_postback) => {
+
+}
+
+// Sends response messages via the Send API
+const callSendAPI = (sender_psid, response) => {
+  let request_body = {
+    "recipient": {
+      "id": sender_psid,
+    },
+    "message": response,
+  }; 
+
+  request({
+    "uri": "https://graph.facebook.com/v2.6/me/messages",
+    "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    if (!err) {
+      console.log('message sent!')
+    } else {
+      console.error("Unable to send message:" + err);
+    }
+  }); 
+  
+}
 
 module.exports = app;
--- server.js
+++ server.js
@@ -4,20 +4,6 @@ const routes = require('./app/routes');
 const PORT = process.env.PORT || 3000;
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
-// Handles messages events
-const handleMessage = (sender_psid, received_message) => {
-
-}
-
-// Handles messaging_postbacks events
-const handlePostback = (sender_psid, received_postback) => {
-
-}
-
-// Sends response messages via the Send API
-const callSendAPI = (sender_psid, response) => {
-  
-}
 
 routes.listen(PORT, () => {
   console.log(`webhook is listening 🔥`);
