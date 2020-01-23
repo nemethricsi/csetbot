e00fefdcc7a8a21b0bbcbbca8ddfdf9250b47c1d
Richárd Németh <nemethricsi@gmail.com>
2020-01-23 10:50:55 +0100

    add PORT

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const routes = require('./app/routes');
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 routes.listen(PORT, () => {
   console.log(`Server running on port ${PORT} 🔥`);
