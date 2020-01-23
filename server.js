83bd85c35509cc19ef65f35f08961e8fcdd47ad4
Richárd Németh <nemethricsi@gmail.com>
2020-01-23 11:59:46 +0100

    add PAGE_ACCESS_TOKEN

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const routes = require('./app/routes');
 const PORT = process.env.PORT || 3000;
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
 routes.listen(PORT, () => {
   console.log(`webhook is listening 🔥`);
